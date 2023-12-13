ca7c8da30dcce4a90de84159b56ccb38f257f799
Tajmaha8849 <shubhamprajapati9537@gmail.com>
2023-12-13 22:43:08 +0530

    Your message

---
+++ NGE.py
@@ -0,0 +1,17 @@
+arr=[100,24,24,1,5]
+max_arr,lst=[],[]
+for i in range(0,len(arr)):
+    j=arr[i]
+    if(i==(len(arr)-1)):
+        lst.append(-1)
+        break
+    else:
+        for k in range(i+1,len(arr)):
+            if(j<arr[k]):
+                max_arr.append(arr[k])
+            else:
+                max_arr.append(-1)
+    lst.append(min(max_arr))
+    max_arr=[]
+print(lst)
+    
